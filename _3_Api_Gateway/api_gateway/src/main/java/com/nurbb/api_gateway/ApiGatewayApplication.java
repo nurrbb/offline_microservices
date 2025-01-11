2a766e2c4f86d4c76805486395a4fade707498b8
nurrbb <enurb320@gmailc.om>
2025-01-11 13:35:32 +0300

    main

---
+++ _3_Api_Gateway/api_gateway/src/main/java/com/nurbb/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.nurbb.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//api gateway
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
