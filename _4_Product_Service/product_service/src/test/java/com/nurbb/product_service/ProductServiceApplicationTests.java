c8dfccdcb1696d98ee5ad2525b5c037b6b1f15df
nurrbb <enurb320@gmailc.om>
2025-01-11 19:52:26 +0300

    Application.properties

---
+++ _4_Product_Service/product_service/src/test/java/com/nurbb/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurbb.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
